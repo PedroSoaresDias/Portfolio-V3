c26dd72e3b19cb143ca5f8e5fb222c828800b485
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-05-10 16:42:43 -0300

    Adicionando animações no Navbar com o Framer Motion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { AiOutlineMenu } from "react-icons/ai";
 import { RxCross2 } from "react-icons/rx";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +14,12 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="bg-gray-950 fixed w-full">
+    <motion.nav
+      className="bg-gray-950 fixed w-full"
+      initial={{ y: -100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -23,9 +29,24 @@ export default function Navbar() {
           </div>
           <div className="hidden md:block">
             <div className="ml-4 flex items-center space-x-4">
-              <Link href={"/#inicio"} className="text-white transition-all duration-300 hover:text-purple-600">Inicio</Link>
-              <Link href={"/#skills"} className="text-white transition-all duration-300 hover:text-purple-600">Skills</Link>
-              <Link href={"/#projetos"} className="text-white transition-all duration-300 hover:text-purple-600">Projetos</Link>
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <Link href={"/#inicio"} className="text-white transition-all duration-300 hover:text-purple-600">Inicio</Link>
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <Link href={"/#skills"} className="text-white transition-all duration-300 hover:text-purple-600">Skills</Link>
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <Link href={"/#projetos"} className="text-white transition-all duration-300 hover:text-purple-600">Projetos</Link>
+              </motion.div>
             </div>
           </div>
           <div className="md:hidden flex items-center">
@@ -40,14 +61,34 @@ export default function Navbar() {
         </div>
       </div>
       {isOpen && (
-        <div className="md:hidden">
+        <motion.div
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.2 }}
+          className="md:hidden"
+        >
           <div className="px-2 pt-2 pb-3 space-y-2 sm:px-3">
-            <Link href={"/#inicio"} className="text-white block transition-all duration-300 hover:text-purple-600">Inicio</Link>
-            <Link href={"/#skills"} className="text-white block transition-all duration-300 hover:text-purple-600">Skills</Link>
-            <Link href={"/#projetos"} className="text-white block transition-all duration-300 hover:text-purple-600">Projetos</Link>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <Link href={"/#inicio"} className="text-white block transition-all duration-300 hover:text-purple-600">Inicio</Link>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <Link href={"/#skills"} className="text-white block transition-all duration-300 hover:text-purple-600">Skills</Link>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <Link href={"/#projetos"} className="text-white block transition-all duration-300 hover:text-purple-600">Projetos</Link>
+            </motion.div>
           </div>
-        </div>
+        </motion.div>
       )}
-    </nav>
+    </motion.nav>
   )
 }
