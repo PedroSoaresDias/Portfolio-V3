e5fb456fba49ffdbcc3b1d7a21b79436ac0819af
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-05-13 07:51:42 -0300

    Aumentando o padding-top com componente Inicio

--- src/components/Inicio.tsx
+++ src/components/Inicio.tsx
@@ -3,7 +3,7 @@ import RedesSociais from "./RedesSociais"
 
 export default function Inicio() {
   return (
-    <section id="inicio" className="flex justify-center items-center min-h-screen bg-gradient-to-tr from-gray-800 to-black pt-14">
+    <section id="inicio" className="flex justify-center items-center min-h-screen bg-gradient-to-tr from-gray-800 to-black pt-16">
       <div className="container flex flex-col justify-center items-center gap-3 md:mx-6 lg:mx-9 text-white py-6 text-center">
         <Image src={"/images/foto-perfil.jpg"} alt="Foto do Pedro Dias" width={200} height={200} className="rounded-full" />
         <RedesSociais />
